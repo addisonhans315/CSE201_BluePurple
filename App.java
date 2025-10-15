867e5686b9a1da1581ee83ef831ad5ff0f8bbf7f
addisonhans315 <hansam2@miamioh.edu>
2025-10-15 11:02:00 -0400

    Add files via upload
    Added exceptions for negative price

--- App.java
+++ App.java
@@ -1,58 +1,58 @@
+package appStore;
+
 public class App {
 	// attributes
 	private int price;
 	private String developer;
 	private String name;
 	private String type;
-	private String logo; // path to img file
-
-	public App(int price, String developer, String name, String type, String logo) {
+	private String logo;  // path to img file
+	
+	public App(int price, String developer, String name, String type, String logo) throws Exception{
 		this.price = price;
+		if (price < 0) {
+			throw new Exception("Invalid price");
+		}
 		this.developer = developer;
 		this.name = name;
 		this.type = type;
 		this.logo = logo;
 	}
-
+	
 	// Getters + Setters
 	public int getPrice() {
 		return price;
 	}
-
-	public void setPrice(int price) {
+	public void setPrice(int price) throws Exception{
+		if (price < 0) {
+			throw new Exception("Invalid price");
+		}
 		this.price = price;
 	}
-
 	public String getDeveloper() {
 		return developer;
 	}
-
 	public void setDeveloper(String developer) {
 		this.developer = developer;
 	}
-
 	public String getName() {
 		return name;
 	}
-
 	public void setName(String name) {
 		this.name = name;
 	}
-
 	public String getType() {
 		return type;
 	}
-
 	public void setType(String type) {
 		this.type = type;
 	}
-
 	public String getLogo() {
 		return logo;
 	}
-
 	public void setLogo(String logo) {
 		this.logo = logo;
 	}
-
+	
+	
 }
