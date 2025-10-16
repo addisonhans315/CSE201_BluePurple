73243bba663e5b538da80d603c3c7f615ea724bf
addisonhans315 <hansam2@miamioh.edu>
2025-10-16 17:57:43 -0400

    Add files via upload
    Changed Apps to an ArrayList<App> instead of an array

--- AppStore.java
+++ AppStore.java
@@ -1,36 +1,34 @@
+package appStore;
+
+import java.util.*;
 public class AppStore {
 	// attributes
-	private App[] apps;
+	private ArrayList<App> apps;
 	private User[] users;
 	private String[] filters;
-
-	public AppStore(App[] apps, User[] users, String[] filters) {
+	
+	public AppStore(ArrayList<App> apps, User[] users, String[] filters) {
 		this.apps = apps;
 		this.users = users;
 		this.filters = filters;
 	}
-
+	
 	// getters + setters
-	public App[] getApps() {
+	public ArrayList<App> getApps() {
 		return apps;
 	}
-
-	public void setApps(App[] apps) {
+	public void setApps(ArrayList<App> apps) {
 		this.apps = apps;
 	}
-
 	public User[] getUsers() {
 		return users;
 	}
-
 	public void setUsers(User[] users) {
 		this.users = users;
 	}
-
 	public String[] getFilters() {
 		return filters;
 	}
-
 	public void setFilters(String[] filters) {
 		this.filters = filters;
 	}
