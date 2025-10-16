10657bf873e4bbfbb154fffaf0169ccb0e466f9d
addisonhans315 <hansam2@miamioh.edu>
2025-10-16 17:41:54 -0400

    Add files via upload
    Added Admin and Moderator classes that extend User class

---
+++ Admin.java
@@ -0,0 +1,12 @@
+package appStore;
+
+public class Admin extends User {
+
+	public Admin(String username, String password) {
+		super(username, password);
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+
+}
---
+++ Moderator.java
@@ -0,0 +1,10 @@
+package appStore;
+
+public class Moderator extends User {
+
+	public Moderator(String username, String password) {
+		super(username, password);
+		// TODO Auto-generated constructor stub
+	}
+
+}
