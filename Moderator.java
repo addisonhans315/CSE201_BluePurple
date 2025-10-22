bef3f80d747bc52cd433e2412fc43f0fb84441b7
addisonhans315 <hansam2@miamioh.edu>
2025-10-22 11:06:50 -0400

    Add files via upload
    Added constructor

--- Moderator.java
+++ Moderator.java
@@ -4,7 +4,9 @@ public class Moderator extends User {
 
 	public Moderator(String username, String password) {
 		super(username, password);
-		// TODO Auto-generated constructor stub
+	}
+	public Moderator(User u) {
+		super(u.getUsername(), u.getPassword());
 	}
 
 }
